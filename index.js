626f997f902486bd6444fe052a9d85049659e7d5
David Borah <dborah87@gmail.com>
2018-07-26 20:07:04 -0400

    hid our secrets. prepared for heroku deployment. write the proxy route for weather data

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const server = express();
+const axios = require('axios');
+
+const urlFormatter = require('url');
+
+//load environment variables
+require('dotenv').config();
 
 //define the port -> heroku needs the first part
 const port = process.env.PORT || 9009;
+
+//define the base url for darksky (the common parts)
+const apikey = process.env.API_KEY;
+const url = `https://api.darksky.net/forecast/${apikey}/`;
 
 //import middleware (power ups)
 const cors = require('cors');
@@ -12,7 +22,17 @@ server.use(cors());
 
 //routes and stuff...
 server.get('/forcast/location/:lat,:lon', (request, response) => {
-    response.send(`it works yayyy!!! ${request.params.lat}`)
+    const { lat, lon } = request.params;
+    const requestUrl = urlFormatter.resolve(url, `${lat},${lon}`);
+    axios.get(requestUrl)
+        .then((weather) => {
+            response.status(200).json(weather.data);
+        })
+        .catch((error) => {
+            response.status(500).json({
+                msg: "don't look now, but there is a Tornado behind you!"
+            });
+        });
 });
 
 //kick off this jam
